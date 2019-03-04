dff963218e54bca8f0b41737faefceaadb469fb6
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-03-04 15:23:51 +0000

    validations done

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,3 +1,5 @@
 class Instruction < ApplicationRecord
   belongs_to :step
+
+  validates :title, :content, :step, presence: true
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :task
+  has_many :instructions
+
+  validates :title, :task, presence: true
 end
