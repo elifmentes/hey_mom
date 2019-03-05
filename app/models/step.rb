a60224b7f76c5382bbe848fbdac4e6225a675223
Mona Chung <monalizards@gmail.com>
2019-03-05 12:47:03 +0000

    Another migration, add content in steps

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,5 +1,5 @@
 class Step < ApplicationRecord
   belongs_to :task
 
-  validates :title, :task, presence: true
+  validates :title, :task, :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_111947) do
+ActiveRecord::Schema.define(version: 2019_03_05_124430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2019_03_05_111947) do
   create_table "steps", force: :cascade do |t|
     t.bigint "task_id"
     t.string "title"
-    t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["task_id"], name: "index_steps_on_task_id"
   end
 
