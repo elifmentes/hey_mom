b94025780c670f5b4822ded46bd5c77e7783a2c9
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-03-06 17:45:18 +0000

    put the search bar in categories index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :categories, only: [:index]
+  resources :tasks, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories, only: [:show] do
     resources :tasks, only: [:show] do
