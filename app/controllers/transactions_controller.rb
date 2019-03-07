43fda2fea870113d08818ea241f98c78bc6b66dc
Mona Chung <monalizards@gmail.com>
2019-03-07 02:14:09 +0000

    Transaction monthly summary display

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,18 @@
 class TransactionsController < ApplicationController
   before_action :set_transactions
   def index
+    @monthly_income = calculate_balance(false)
+    @monthly_expense = calculate_balance(true)
   end
 
   private
 
   def set_transactions
     @transactions = Transaction.where(user: current_user)
+  end
+
+  def calculate_balance(expense)
+    @subtransactions = @transactions.where('expense = ? AND created_at > ?', expense, 1.month.ago)
+    return @subtransactions.map{ |t| t.amount }.sum
   end
 end
