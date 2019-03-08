9ad4b9f94c6e30784e86ecf74f5020d04a3ca70d
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-03-08 12:40:29 +0000

    basic weather api

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @user = current_user
+    @weather = Rails.configuration.open_weather_api.current city: @user.location if user_signed_in?
   end
 
   def show
