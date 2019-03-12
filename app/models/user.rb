0956b1abccb8f04b5dd084d3aec3f31c7b11b501
Beatriz Barroso <beatrizscnbarroso@gmail.com>
2019-03-12 14:37:34 +0000

    added geocoder to user location

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
   has_many :tasks, through: :user_tasks
   has_many :transactions
 
+  geocoded_by :location
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :gender, inclusion: { in: [true, false] }, default: false
   validates :location, presence: true
+
+  after_validation :geocode, if: :will_save_change_to_location?
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
