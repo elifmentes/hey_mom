204367203fbd2e5b605f40e5cd0be8b99ab0fd01
Mona Chung <monalizards@gmail.com>
2019-03-13 17:09:43 +0000

    Change to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-require "open-uri"
-require "JSON"
+# require "open-uri"
+# require "JSON"
 
+Transaction.destroy_all
 UserTask.destroy_all
 User.destroy_all
 Step.destroy_all
@@ -218,7 +219,6 @@ housekeeping_articles.each { |article| url_single(housekeeping, "#{base_url}#{ar
 productivity_articles.each { |article| url_single(productivity, "#{base_url}#{article}") }
 relationship_articles.each { |article| url_single(relationship, "#{base_url}#{article}") }
 
-Transaction.destroy_all
 t = Transaction.create(user: admin, category: "Food", description: "Lunching with birds", amount: 55)
 t = Transaction.create(user: admin, description: "Work in pub", amount: 15, expense: false)
 t = Transaction.create(user: admin, category: "Food", description: "Lunching with dogs", amount: 65)
