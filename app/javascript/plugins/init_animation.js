1acbaf88754c6eb42d05b968e3a3d16cdfd67a4c
Mona Chung <monalizards@gmail.com>
2019-03-14 15:16:56 +0000

    Fix animate

--- app/javascript/plugins/init_animation.js
+++ app/javascript/plugins/init_animation.js
@@ -1,14 +1,14 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
-const dropdown = document.querySelector(".dropdown")
-const dropdownMenu = document.querySelector(".dropdown-menu")
+// const dropdown = document.querySelector(".dropdown")
+// const dropdownMenu = document.querySelector(".dropdown-menu")
 
-const animated = () => {
-  $('#slide').on('click', function() {
-    $(this).find('.dropdown-menu').animate({width: 'toggle'}, 120);
-  });
+// const animated = () => {
+//   $('#slide').on('click', function() {
+//     $(this).find('.dropdown-menu').animate({width: 'toggle'}, 120);
+//   });
 
-};
+// };
 
-export { animated }
+// export { animated }
 
